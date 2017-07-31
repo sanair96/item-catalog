00264edb7fa55662d8d59596f01727d45f1d79e9
sanair96 <sandeep_sashok@hotmail.com>
2017-07-31 18:09:25 +0530

    Done adding requirements, working on suggesstions

--- project.py
+++ project.py
@@ -1,6 +1,11 @@
 
-from flask import Flask, render_template, request, redirect, jsonify, \
-    url_for, flash
+from flask import (Flask,
+                    render_template, 
+                    request, 
+                    redirect, 
+                    jsonify,
+                    url_for, 
+                    flash)
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem, User
